1b659a71e3fbf2656eb48615250909a9106bb992
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-07 15:41:24 -0400

    Made 3 new variables. One for the range between 1 through 15. Second for the maximum guess the user can get wrong. An the third varible is just assigned for no wrong guess so it's asigned to be zero.

--- Lab3_awilliams#2.py
+++ Lab3_awilliams#2.py
@@ -67,3 +67,13 @@ hangman_stages = ["""  -----
                   ---------
                   """
 ]
+
+number_to_guess = random.randint(1,15) 
+
+max_wrong_guesses = len(hangman_stages)- 1
+
+wrong_guesses = 0
+
+print('Come on and play Guess The Number - Hangman Game!')
+print('Guess a number between 1 and 15.')
+
