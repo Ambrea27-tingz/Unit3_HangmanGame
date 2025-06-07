32b4384e83c0ae503ad3c21aa5daa214a2778aa8
Ambrea27-tingz <ambreawilliams97@gmail.com>
2025-06-07 15:32:08 -0400

    Created a new file for this project and imported the random function before setting up the game.

---
+++ Lab3_awilliams#2.py
@@ -0,0 +1,10 @@
+"""Name: Ambrea Williams
+   
+   Date: 06/07/2025
+
+   Unit 3: Lab 3
+
+   Description: Import the file from project 1, do not copy and paste it.  Append 5 more items to the list.  Print the result.
+   """
+
+import random
